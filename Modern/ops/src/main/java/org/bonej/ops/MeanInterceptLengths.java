5110dc0fffe2320b849432c5ce6cd835a0e72750
Richard Domander <rdomander@rvc.ac.uk>
2017-09-29 17:16:20 +0100

    Refactor to Apache Commons math

--- Modern/ops/src/main/java/org/bonej/ops/MeanInterceptLengths.java
+++ Modern/ops/src/main/java/org/bonej/ops/MeanInterceptLengths.java
@@ -12,6 +12,7 @@ import java.util.Random;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import net.imagej.ops.Contingent;
 import net.imagej.ops.Op;
 import net.imagej.ops.special.function.AbstractUnaryFunctionOp;
 import net.imglib2.RandomAccess;
@@ -20,12 +21,12 @@ import net.imglib2.type.BooleanType;
 import net.imglib2.type.numeric.NumericType;
 import net.imglib2.util.ValuePair;
 
+import org.apache.commons.math3.complex.Quaternion;
+import org.apache.commons.math3.geometry.euclidean.threed.Vector3D;
+import org.apache.commons.math3.random.UnitSphereRandomVectorGenerator;
 import org.scijava.ItemIO;
 import org.scijava.plugin.Parameter;
 import org.scijava.plugin.Plugin;
-import org.scijava.vecmath.Quat4d;
-import org.scijava.vecmath.Tuple3d;
-import org.scijava.vecmath.Vector3d;
 
 /**
  * Returns a cloud of MIL vectors
@@ -34,29 +35,34 @@ import org.scijava.vecmath.Vector3d;
  * @author Michael Doube
  */
 // TODO Implement for 2D
-// TODO implements Contingent
-// TODO Use RealVector
-// TODO Depend on Apache Commons Math
 @Plugin(type = Op.class)
 public class MeanInterceptLengths<B extends BooleanType<B>> extends
-	AbstractUnaryFunctionOp<RandomAccessibleInterval<B>, Collection<Tuple3d>>
+	AbstractUnaryFunctionOp<RandomAccessibleInterval<B>, Collection<Vector3D>>
+	implements Contingent
 {
 
+	/**
+	 * Generates uniformly sampled random unit quaternions, which represent
+	 * uniformly sampled random rotations.
+	 */
+	private static final UnitSphereRandomVectorGenerator generator =
+		new UnitSphereRandomVectorGenerator(4);
 	private static final Random random = new Random();
 
-	/** Number of sampling vectors on each plane */
+	/** Number of sampling vectors on each plane. */
 	@Parameter(required = false)
 	private Long vectors = null;
 
-	/** Increment added to the scalar value t used to scale sampling vectors */
+	/** Increment added to the scalar value t used to scale sampling vectors. */
 	@Parameter(required = false)
 	private double tIncrement = 1.0 / 2.3;
 
+	/** The unit quaternion used to rotate the sampling grid. */
 	@Parameter(type = ItemIO.BOTH, required = false)
-	private Quat4d rotation = null;
+	private double[] rotation = null;
 
 	@Override
-	public Collection<Tuple3d> calculate(
+	public Collection<Vector3D> calculate(
 		final RandomAccessibleInterval<B> interval)
 	{
 		final long[] bounds = findBounds(interval);
@@ -67,16 +73,23 @@ public class MeanInterceptLengths<B extends BooleanType<B>> extends
 		}
 		if (rotation == null) {
 			// TODO Test how ItemIO.BOTH works
-			rotation = nextQuaternion();
+			rotation = generator.nextVector();
 		}
-		final Vector3d centroid = findCentroid(bounds);
+		final Vector3D centroid = findCentroid(bounds);
 		final SamplingGrid grid = new SamplingGrid(gridSize, rotation, centroid);
-		final Stream<ValuePair<Vector3d, Vector3d>> samplers = grid.getSamplers(
+		final Stream<ValuePair<Vector3D, Vector3D>> samplers = grid.getSamplers(
 			vectors);
 		return samplers.map(sampler -> sample(interval, bounds, sampler,
 			tIncrement)).filter(Objects::nonNull).collect(Collectors.toList());
 	}
 
+	// TODO Check RAI dimensionality
+	@Override
+	public boolean conforms() {
+		return rotation == null || rotation.length == 4;
+	}
+
+	// region -- Helper methods (public static for unit testing) --
 	public static <C extends NumericType<C>> long[] findBounds(
 		final RandomAccessibleInterval<C> interval)
 	{
@@ -85,26 +98,26 @@ public class MeanInterceptLengths<B extends BooleanType<B>> extends
 		return bounds;
 	}
 
-	public static ValuePair<Double, Double> findBoxIntersections(
-		final ValuePair<Vector3d, Vector3d> sampler, final long[] bounds)
+	public static ValuePair<Double, Double> findIntervalIntersections(
+		final ValuePair<Vector3D, Vector3D> sampler, final long[] bounds)
 	{
-		final Vector3d origin = sampler.a;
-		final Vector3d direction = sampler.b;
+		final Vector3D origin = sampler.a;
+		final Vector3D direction = sampler.b;
 		// Subtract epsilon from bounds, so that vectors don't intersect the stack
 		// at w,h or d which would cause an index out of bounds exception
 		final double eps = 1e-12;
-		final double minX = direction.x >= 0.0 ? 0 : bounds[0] - eps;
-		final double maxX = direction.x >= 0.0 ? bounds[0] - eps : 0;
-		final double minY = direction.y >= 0.0 ? 0 : bounds[1] - eps;
-		final double maxY = direction.y >= 0.0 ? bounds[1] - eps : 0;
-		final double minZ = direction.z >= 0.0 ? 0 : bounds[2] - eps;
-		final double maxZ = direction.z >= 0.0 ? bounds[2] - eps : 0;
-		final double tX0 = (minX - origin.x) / direction.x;
-		final double tX1 = (maxX - origin.x) / direction.x;
-		final double tY0 = (minY - origin.y) / direction.y;
-		final double tY1 = (maxY - origin.y) / direction.y;
-		final double tZ0 = (minZ - origin.z) / direction.z;
-		final double tZ1 = (maxZ - origin.z) / direction.z;
+		final double minX = direction.getX() >= 0.0 ? 0 : bounds[0] - eps;
+		final double maxX = direction.getX() >= 0.0 ? bounds[0] - eps : 0;
+		final double minY = direction.getY() >= 0.0 ? 0 : bounds[1] - eps;
+		final double maxY = direction.getY() >= 0.0 ? bounds[1] - eps : 0;
+		final double minZ = direction.getZ() >= 0.0 ? 0 : bounds[2] - eps;
+		final double maxZ = direction.getZ() >= 0.0 ? bounds[2] - eps : 0;
+		final double tX0 = (minX - origin.getX()) / direction.getX();
+		final double tX1 = (maxX - origin.getX()) / direction.getX();
+		final double tY0 = (minY - origin.getY()) / direction.getY();
+		final double tY1 = (maxY - origin.getY()) / direction.getY();
+		final double tZ0 = (minZ - origin.getZ()) / direction.getZ();
+		final double tZ1 = (maxZ - origin.getZ()) / direction.getZ();
 		if (tX0 > tY1 || tY0 > tX1) {
 			return null;
 		}
@@ -122,8 +135,8 @@ public class MeanInterceptLengths<B extends BooleanType<B>> extends
 		return new ValuePair<>(tMin, tMax);
 	}
 
-	public static Vector3d findCentroid(final long[] bounds) {
-		return new Vector3d(bounds[0] * 0.5, bounds[1] * 0.5, bounds[2] * 0.5);
+	public static Vector3D findCentroid(final long[] bounds) {
+		return new Vector3D(bounds[0] * 0.5, bounds[1] * 0.5, bounds[2] * 0.5);
 	}
 
 	public static double findGridSize(final long[] bounds) {
@@ -132,11 +145,11 @@ public class MeanInterceptLengths<B extends BooleanType<B>> extends
 	}
 
 	public static <B extends BooleanType<B>> long getElement(
-		final RandomAccess<B> access, final Vector3d position)
+		final RandomAccess<B> access, final Vector3D position)
 	{
-		access.setPosition((int) position.x, 0);
-		access.setPosition((int) position.y, 1);
-		access.setPosition((int) position.z, 2);
+		access.setPosition((int) position.getX(), 0);
+		access.setPosition((int) position.getY(), 1);
+		access.setPosition((int) position.getZ(), 2);
 		return (long) access.get().getRealDouble();
 	}
 
@@ -166,13 +179,13 @@ public class MeanInterceptLengths<B extends BooleanType<B>> extends
 		return Math.min(a, b);
 	}
 
-	public static <B extends BooleanType<B>> Vector3d sample(
+	public static <B extends BooleanType<B>> Vector3D sample(
 		final RandomAccessibleInterval<B> interval, final long[] stack,
-		final ValuePair<Vector3d, Vector3d> sampler, final double increment)
+		final ValuePair<Vector3D, Vector3D> sampler, final double increment)
 	{
 		// TODO Refactor to findMILVector(sampler) { intersections = interfaces
 		// ... return milVector;}
-		final ValuePair<Double, Double> tPair = findBoxIntersections(sampler,
+		final ValuePair<Double, Double> tPair = findIntervalIntersections(sampler,
 			stack);
 		if (tPair == null) {
 			return null;
@@ -184,56 +197,31 @@ public class MeanInterceptLengths<B extends BooleanType<B>> extends
 			// interval fits (at most) one sampling point. no intersections
 			return null;
 		}
-		final Vector3d origin = sampler.a;
-		final Vector3d direction = sampler.b;
-		final Vector3d jitter = new Vector3d(direction);
-		jitter.scale(jitterT);
-		final Vector3d point = new Vector3d();
-		point.scaleAdd(tMin, direction, origin);
-		point.add(jitter);
-		final Vector3d bit = new Vector3d(direction);
-		bit.scale(increment);
+		final Vector3D origin = sampler.a;
+		final Vector3D direction = sampler.b;
+		final Vector3D jitter = direction.scalarMultiply(jitterT);
+		final Vector3D samplingGap = direction.scalarMultiply(increment);
 		final RandomAccess<B> access = interval.randomAccess();
+		Vector3D point = direction.scalarMultiply(tMin).add(origin).add(jitter);
 		long previous = getElement(access, point);
 		long interfaces = 0;
 		for (double t = tMin; t <= tMax; t += increment) {
 			final long current = getElement(access, point);
 			interfaces += current ^ previous;
-			point.add(bit);
+			point.add(samplingGap);
 			previous = current;
 		}
 		return toMILVector(direction, tPair, interfaces);
 	}
 
-	public static Vector3d toMILVector(final Vector3d direction,
+	public static Vector3D toMILVector(final Vector3D direction,
 		final ValuePair<Double, Double> tPair, final long intersections)
 	{
-		if (intersections == 0) {
-			return direction;
+		final double intervalScalar = Math.abs(tPair.a - tPair.b);
+		if (intersections < 2) {
+			return direction.scalarMultiply(intervalScalar);
 		}
-		final double t = Math.abs(tPair.a - tPair.b) / intersections;
-		final Vector3d milVector = new Vector3d(direction);
-		milVector.scale(t);
-		return milVector;
-	}
-
-	// region -- Helper methods --
-
-	/**
-	 * Creates a random unit quaternion which represents a rotation
-	 * <p>
-	 * The quaternion is first assigned four normally distributed coordinates with
-	 * mean {@code 0.0} and standard deviation {@code 1.0}. Then it's normalised.
-	 * This method guarantees that the unit quaternions are uniformly sampled, and
-	 * they represent uniformly sampled random rotations.
-	 * </p>
-	 */
-	private Quat4d nextQuaternion() {
-		final double x = random.nextGaussian();
-		final double y = random.nextGaussian();
-		final double z = random.nextGaussian();
-		final double w = random.nextGaussian();
-		return new Quat4d(x, y, z, w);
+		return direction.scalarMultiply(intervalScalar / intersections);
 	}
 
 	// endregion
@@ -246,16 +234,16 @@ public class MeanInterceptLengths<B extends BooleanType<B>> extends
 		private final SamplingPlane xz;
 		private final SamplingPlane yz;
 
-		public SamplingGrid(final double gridSize, final Quat4d quaternion,
-			final Tuple3d centroid)
+		public SamplingGrid(final double gridSize, final double[] quaternion,
+			final Vector3D centroid)
 		{
 			xy = new SamplingPlane(gridSize, XY, quaternion, centroid);
 			xz = new SamplingPlane(gridSize, XZ, quaternion, centroid);
 			yz = new SamplingPlane(gridSize, YZ, quaternion, centroid);
 		}
 
-		public Stream<ValuePair<Vector3d, Vector3d>> getSamplers(final long n) {
-			final Stream.Builder<ValuePair<Vector3d, Vector3d>> builder = Stream
+		public Stream<ValuePair<Vector3D, Vector3D>> getSamplers(final long n) {
+			final Stream.Builder<ValuePair<Vector3D, Vector3D>> builder = Stream
 				.builder();
 			for (long i = 0; i < n; i++) {
 				builder.accept(xy.getSampler());
@@ -268,72 +256,71 @@ public class MeanInterceptLengths<B extends BooleanType<B>> extends
 
 	public static final class SamplingPlane {
 
-		private final Vector3d translation;
-		private final Vector3d u;
-		private final Vector3d v;
-		private final Vector3d normal;
+		private Vector3D translation;
+		private Vector3D u;
+		private Vector3D v;
+		private Vector3D normal;
 
 		private SamplingPlane(final double gridSize, final Orientation orientation,
-			final Quat4d quaternion, final Tuple3d centroid)
+			final double[] quaternion, final Vector3D centroid)
 		{
-			final int direction = random.nextBoolean() ? 1 : -1;
+			createVectors(orientation, gridSize);
+			rotateVectors(quaternion);
+			translation = translation.add(centroid);
+		}
+
+		private void createVectors(final Orientation orientation,
+			final double gridSize)
+		{
+			final int orthogonalDirection = random.nextBoolean() ? 1 : -1;
 			final double halfGrid = -0.5 * gridSize;
-			final double planeShift = direction * halfGrid;
+			final double orthogonalShift = orthogonalDirection * halfGrid;
 			if (orientation == XY) {
-				u = new Vector3d(gridSize, 0, 0);
-				v = new Vector3d(0, gridSize, 0);
-				normal = new Vector3d(0, 0, direction);
-				translation = new Vector3d(halfGrid, halfGrid, planeShift);
+				u = new Vector3D(gridSize, 0, 0);
+				v = new Vector3D(0, gridSize, 0);
+				normal = new Vector3D(0, 0, orthogonalDirection);
+				translation = new Vector3D(halfGrid, halfGrid, orthogonalShift);
 			}
 			else if (orientation == XZ) {
-				u = new Vector3d(gridSize, 0, 0);
-				normal = new Vector3d(0, direction, 0);
-				v = new Vector3d(0, 0, gridSize);
-				translation = new Vector3d(halfGrid, planeShift, halfGrid);
+				u = new Vector3D(gridSize, 0, 0);
+				normal = new Vector3D(0, orthogonalDirection, 0);
+				v = new Vector3D(0, 0, gridSize);
+				translation = new Vector3D(halfGrid, orthogonalShift, halfGrid);
 			}
 			else if (orientation == YZ) {
-				normal = new Vector3d(direction, 0, 0);
-				u = new Vector3d(0, gridSize, 0);
-				v = new Vector3d(0, 0, gridSize);
-				translation = new Vector3d(planeShift, halfGrid, halfGrid);
+				normal = new Vector3D(orthogonalDirection, 0, 0);
+				u = new Vector3D(0, gridSize, 0);
+				v = new Vector3D(0, 0, gridSize);
+				translation = new Vector3D(orthogonalShift, halfGrid, halfGrid);
 			}
 			else {
 				throw new IllegalArgumentException("Unexpected orientation");
 			}
-
-			rotateVectors(quaternion);
-			translation.add(centroid);
 		}
 
-		private ValuePair<Vector3d, Vector3d> getSampler() {
+		private ValuePair<Vector3D, Vector3D> getSampler() {
 			final double uScale = random.nextDouble();
 			final double vScale = random.nextDouble();
-			final double x = uScale * u.x + vScale * v.x;
-			final double y = uScale * u.y + vScale * v.y;
-			final double z = uScale * u.z + vScale * v.z;
-			final Vector3d origin = new Vector3d(x, y, z);
+			final double x = uScale * u.getX() + vScale * v.getX();
+			final double y = uScale * u.getY() + vScale * v.getY();
+			final double z = uScale * u.getZ() + vScale * v.getZ();
+			final Vector3D origin = new Vector3D(x, y, z);
 			origin.add(translation);
 			return new ValuePair<>(origin, normal);
 		}
 
-		// TODO Op?
-		private static void rotate(final Vector3d v, final Quat4d q) {
-			final Quat4d p = new Quat4d();
-			final Quat4d qInv = new Quat4d();
-			final Quat4d rotated = new Quat4d(q);
-			p.set(v.x, v.y, v.z, 0.0);
-			rotated.mul(p);
-			// No need to divide by the inverse of q since it's a unit quaternion
-			qInv.set(-q.x, -q.y, -q.z, q.w);
-			rotated.mul(qInv);
-			v.set(rotated.x, rotated.y, rotated.z);
+		private static Vector3D rotate(final Vector3D v, final Quaternion q) {
+			final Quaternion p = new Quaternion(0.0, v.toArray());
+			final Quaternion rotated = q.multiply(p).multiply(q.getInverse());
+			return new Vector3D(rotated.getVectorPart());
 		}
 
-		private void rotateVectors(final Quat4d quaternion) {
-			rotate(translation, quaternion);
-			rotate(u, quaternion);
-			rotate(v, quaternion);
-			rotate(normal, quaternion);
+		private void rotateVectors(final double[] quaternion) {
+			final Quaternion q = new Quaternion(quaternion);
+			translation = rotate(translation, q);
+			u = rotate(u, q);
+			v = rotate(v, q);
+			normal = rotate(normal, q);
 		}
 
 		public enum Orientation {
